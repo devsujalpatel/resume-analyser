ffaa438ce925eca00116d02f0d8a8552a31ce093
sujalpatelcoder <sujal7455@gmail.com>
2025-07-19 18:57:42 +0530

    feat:authentication done

--- app/routes/Auth.tsx
+++ app/routes/Auth.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
 import { usePuterStore } from "~/lib/puter";
 
 export const meta = () => [
@@ -6,7 +8,14 @@ export const meta = () => [
 ];
 
 const Auth = () => {
-  const { isLoading } = usePuterStore();
+  const { isLoading, auth } = usePuterStore();
+  const location = useLocation();
+  const next = location.search.split("next=")[1];
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (auth.isAuthenticated) navigate(next);
+  }, [auth.isAuthenticated, next]);
 
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover min-h-screen flex items-center justify-center">
@@ -15,6 +24,25 @@ const Auth = () => {
           <div>
             <h1>Welcome</h1>
             <h2>Log In to Continue Your Job Journey</h2>
+          </div>
+          <div>
+            {isLoading ? (
+              <button className="auth-button animate-pulse">
+                <p>Signing you in...</p>
+              </button>
+            ) : (
+              <>
+                {auth.isAuthenticated ? (
+                  <button className="auth-button" onClick={auth.signOut}>
+                    <p>Log out</p>
+                  </button>
+                ) : (
+                  <button className="auth-button" onClick={auth.signIn}>
+                    <p>Log in</p>
+                  </button>
+                )}
+              </>
+            )}
           </div>
         </section>
       </div>
