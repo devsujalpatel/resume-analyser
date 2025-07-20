35d5aa4a82e8c006d9b220caebbdef9d9dd3ab5b
sujalpatelcoder <sujal7455@gmail.com>
2025-07-20 15:28:34 +0530

    fix

--- app/routes.ts
+++ app/routes.ts
@@ -1,9 +1,9 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-  index("routes/Home.tsx"),
+  index("routes/home.tsx"),
   route("/auth", "routes/Auth.tsx"),
-  route("/upload", "routes/Upload.tsx"),
-  route("/resume/:id", "routes/Resume.tsx"),
-  route("/wipe", "routes/Wipe.tsx"),
+  route("/upload", "routes/upload.tsx"),
+  route("/resume/:id", "routes/resume.tsx"),
+  route("/wipe", "routes/wipe.tsx"),
 ] satisfies RouteConfig;
